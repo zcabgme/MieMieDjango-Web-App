67a90345bfeee3da9d4e9c21ce6a0277f6451b23
thatguy1104 <30369753+thatguy1104@users.noreply.github.com>
2021-08-18 11:59:17 +0100

    attempt to fix deployment error

--- core/settings.py
+++ core/settings.py
@@ -72,8 +72,6 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': BASE_DIR + '/' + 'db.sqlite3',
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'summermiemiepostgreserver',
         'HOST': 'summermiemiepostgreserver.postgres.database.azure.com',
