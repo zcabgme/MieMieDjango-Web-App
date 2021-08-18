b5fe3d13fec67db378d9ecfde4779fc026028ce3
thatguy1104 <30369753+thatguy1104@users.noreply.github.com>
2021-08-19 00:09:54 +0100

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -20,7 +20,8 @@ SECRET_KEY = config('SECRET_KEY', default='S#perS3crEt_1122')
 DEBUG = config('DEBUG', default=True, cast=bool)
 
 # load production server from .env
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', config('SERVER', default='127.0.0.1'), 'summermiemiedjango.azurewebsites.net']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', config(
+    'SERVER', default='127.0.0.1'), 'summermiemiewebapp.azurewebsites.net']
 
 # Application definition
 
